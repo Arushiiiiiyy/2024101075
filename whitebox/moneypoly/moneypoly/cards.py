32792d0e54c279913cf248d1dfce6ecdd1868f78
Arushi Shukla <arushi.shukla@students.iiit.ac.in>
2026-03-20 19:22:30 +0530

    Iteration 47: cards.py - line ~73 - C:broke the line

--- whitebox/moneypoly/moneypoly/cards.py
+++ whitebox/moneypoly/moneypoly/cards.py
@@ -70,7 +70,9 @@ COMMUNITY_CHEST_CARDS = [
     {"description": "Pay school fees of $50.",                               
      "action": "pay",      
      "value": 50},
-    {"description": "Receive $25 consultancy fee.",                          "action": "collect",  "value": 25},
+    {"description": "Receive $25 consultancy fee.",                          
+     "action": "collect",  
+     "value": 25},
     {"description": "Get Out of Jail Free.",                                 "action": "jail_free","value": 0},
 ]
 
