746f10ef72a4153b22281ecb6c9064976a2ad555
Arushi Shukla <arushi.shukla@students.iiit.ac.in>
2026-03-20 16:17:44 +0530

    Iteration 24: property.py - line 7 - R: refactored the __init__

--- whitebox/moneypoly/moneypoly/property.py
+++ whitebox/moneypoly/moneypoly/property.py
@@ -1,5 +1,4 @@
 """Handles property logic - buying , mortage, selling etc .."""
-# pylint: disable=too-many-instance-attributes
 class Property:
     """Represents a single purchasable property tile on the MoneyPoly board."""
 
@@ -10,15 +9,22 @@ class Property:
         self.position = position
         self.price = price
         self.base_rent = base_rent
-        self.mortgage_value = price // 2
+        #self.mortgage_value = price // 2
         self.owner = None
         self.is_mortgaged = False
-        self.houses = 0
+        #self.houses = 0
 
         # Register with the group immediately on creation
         self.group = group
         if group is not None and self not in group.properties:
             group.properties.append(self)
+    
+    #made a new function so that number of instance attribute decreases 
+    @property
+    def mortgage_value(self):
+        """Return the mortgage payout value (half the purchase price)."""
+        return self.price // 2
+
 
     def get_rent(self):
         """
